6a2ff2d498907b9aa75d5cf921f6a9757b9e2028
mabakry <mabakry@gmail.com>
2024-07-18 08:32:25 +0000

    fix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,12 +6,12 @@
 def rotate_2d_matrix(matrix):
     """Rotates an n by n 2D matrix in place.
     """
-    if type(matrix) != list:
+    """if type(matrix) != list:
         return
     if len(matrix) <= 0:
         return
     if not all(map(lambda x: type(x) == list, matrix)):
-        return
+        return"""
     rows = len(matrix)
     cols = len(matrix[0])
     if not all(map(lambda x: len(x) == cols, matrix)):
